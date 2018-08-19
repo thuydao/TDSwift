4e8aa047030991f66720b4a03826a47384f18810
Thuy.Do <thuy.do@icehousecorp.com>
2018-08-19 11:40:53 +0700

    push test

---
+++ TDSwift/Classes/BaseCustomXibView.swift
@@ -0,0 +1,47 @@
+//
+//  BaseCustomXibView.swift
+//  QHIOS
+//
+//  Created by Điệp Nguyễn on 4/16/18.
+//  Copyright © 2018 QH. All rights reserved.
+//
+
+import UIKit
+class BaseCustomXibView: UIView {
+    var view: UIView!
+    fileprivate(set) var nibName: String!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+        xibSetup()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+        xibSetup()
+    }
+    fileprivate func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.isUserInteractionEnabled = true
+        addSubview(view)
+        didLoadFromNib()
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        if let view = nib.instantiate(withOwner: self, options: nil)[0] as? UIView {
+            return view
+        }
+        return UIView()
+    }
+    
+    func initialize() {
+        nibName = "\(self.classForCoder)"
+    }
+    
+    func didLoadFromNib() {}
+}
