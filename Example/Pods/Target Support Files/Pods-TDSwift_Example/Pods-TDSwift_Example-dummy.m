fac492d573de6776bbccf3c19127fb7d641b2c0a
Thuy.Do <thuy.do@icehousecorp.com>
2018-08-19 11:29:14 +0700

    init project

---
+++ Example/Pods/Target Support Files/Pods-TDSwift_Example/Pods-TDSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TDSwift_Example
+@end
---
+++ Example/Pods/Target Support Files/TDSwift/TDSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TDSwift : NSObject
+@end
+@implementation PodsDummy_TDSwift
+@end
